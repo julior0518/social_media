1e3001bbd044561a6e99d67814a2c6a2f3e56945
julio <julior0518@gmail.com>
2022-03-16 11:26:18 -0400

    added loader spinner and conditional navBar

--- client/src/components/Feed.jsx
+++ client/src/components/Feed.jsx
@@ -1,6 +1,16 @@
+import { useState, useEffect} from 'react'
+import { useParams } from "react-router-dom"
+
+import { client } from '../client'
+import MasonryLayout from './MasonryLayout'
+import Spinner from './Spinner'
+
 
 const Feed = () => {
 
+    const [loading, setLoading ] = useState (false)
+
+    if(!loading) return <Spinner message="feeding to feed feed" />
     return (
         <div>
             Feed
